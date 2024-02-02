e80ab1e063a1125e0b5e9e28954daf4d6f49d1c5
sunskwon <sunskwon@gmail.com>
2024-02-02 14:42:29 +0900

    240202

--- answer/kang/A240131.java
+++ answer/kang/A240131.java
@@ -6,12 +6,21 @@ public class A240131 {
 
     public static void main(String[] args) {
 
-        /* 임의의 정수를 입력받아 5의배수, 7의배수를 출력하는 프로그램을 만드세요.
-
-         * --------------- 출력 예시 ---------------
-         * 숫자를 입력하세요 :
-         * 예1) 입력하신 숫자는 5의 배수입니다.
-         * 예2) 입력하신 숫자는 5의 배수이면서 7의 배수입니다.
+        /* 임의의 정수를 입력받아 3의 배수이면서 5의 배수이면 출력하는 프로그램을 만드세요.
+         *  단, 3의 배수이면서 5의 배수인 경우 프로그램이 종료되어야 한다.
+         * -------- 출력예시 ------
+         * 숫자를 입력하세요 : 15
+         * 입력하신 숫자는 3의 배수이면서 5의 배수입니다.
+         * 프로그램을 종료합니다.
+         *
+         * 숫자를 입력하세요 : 9
+         * 입력하신 숫자는 3의 배수이지만 5의 배수가 아닙니다.
+         *
+         * 숫자를 입력하세요 : 10
+         * 입력하신 숫자는 5의 배수이지만 3의 배수가 아닙니다.
+         *
+         * 숫자를 입력하세요 : -5
+         * 양의 정수를 입력해야 합니다.
          * */
 
         A240131 a = new A240131();
@@ -21,22 +30,27 @@ public class A240131 {
     public void judge() {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("정수를 입력하세요 : ");
+        System.out.print("숫자를 입력하세요 : ");
         int num = sc.nextInt();
 
+        int num3 = num % 3;
         int num5 = num % 5;
-        int num7 = num % 7;
-        int num35 = num % 35;
+        int num15 = num % 15;
 
-        if (num35 == 0) {
+        if (num <= 0) {
 
-            System.out.println("입력하신 숫자는 5의 배수이면서 7의 배수입니다.");
+            System.out.println("양의 정수를 입력해야 합니다.");
+        } else if (num15 == 0) {
+
+            System.out.println("입력하신 숫자는 3의 배수이면서 5의 배수입니다.");
+        } else if (num3 == 0) {
+
+            System.out.println("입력하신 숫자는 3의 배수이지만, 5의 배수가 아닙니다.");
         } else if (num5 == 0) {
 
-            System.out.println("입력하신 숫자는 5의 배수입니다.");
-        } else if (num7 == 0) {
-
-            System.out.println("입력하신 숫자는 7의 배수입니다.");
+            System.out.println("입력하신 숫자는 5의 배수이지만, 3의 배수가 아닙니다.");
         }
+
+        System.out.println("프로그램을 종료합니다.");
     }
 }
